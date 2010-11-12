ddbd8dfc5c9c2e68df357fc8851b2f80848ee4f0
Dan Porter <wolfpakz@gmail.com>
2010-11-11 17:31:07 -0800

    Move delayed_job include

--- lib/analytico/client.rb
+++ lib/analytico/client.rb
@@ -1,3 +1,6 @@
+require 'active_support/core_ext'
+require 'delayed_job'
+
 module Analytico
 
   class Client
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'analytico'
 require 'spec'
 require 'spec/autorun'
-require 'delayed_job'
 
 Spec::Runner.configure do |config|
   config.mock_with :mocha
