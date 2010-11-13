2d4e8d8733d2832c82c1d290419e009aac9e1d94
Mauricio Gomes <mauricio@edge14.com>
2010-11-13 16:32:09 -0500

    Added unique visitor support through cookies

--- lib/analytico/railtie/rack_impression.rb
+++ lib/analytico/railtie/rack_impression.rb
@@ -3,7 +3,37 @@ module AnalyticoController
   private
   
     def analytico_impression
-      Analytico::Client.add_impression(:fqdn => request.host, :ip => request.ip, :user_agent => request.user_agent, :referrer => request.referrer, :url => request.path)
+      if cookies
+        if cookies[:analytico]
+          analytico_cookie = cookies[:analytico]
+        else
+          analytico_cookie = generate_cookie
+          cookies[:analytico] = {
+            :value => analytico_cookie,
+            :expires => 2.years.from_now,
+            :domain => request.host
+          }
+        end
+      
+        Analytico::Client.add_impression(:fqdn => request.host, 
+                                         :ip => request.ip, 
+                                         :user_agent => request.user_agent, 
+                                         :referrer => request.referrer, 
+                                         :url => request.path,
+                                         :cookie => analytico_cookie)
+      else
+        Analytico::Client.add_impression(:fqdn => request.host, 
+                                         :ip => request.ip, 
+                                         :user_agent => request.user_agent, 
+                                         :referrer => request.referrer, 
+                                         :url => request.path)
+      end
+		end
+		
+		def generate_cookie
+		  digest = Digest::MD5.new
+		  digest << "#{Time.now}-#{rand(999999)}"
+		  digest.to_s
 		end
   
 end
