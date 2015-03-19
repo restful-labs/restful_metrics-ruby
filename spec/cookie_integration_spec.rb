edede6dafdf89b2692f5e527f4e633337039e6f3
Dan Porter <wolfpakz@gmail.com>
2015-03-19 15:26:12 -0700

    Updated cookie integration specs

--- spec/cookie_integration_spec.rb
+++ spec/cookie_integration_spec.rb
@@ -2,43 +2,79 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Rails controller integration" do
 
-  class ApplicationController < ActionController::Base
+  class TestController
+    include RestfulMetrics::ControllerMethods::InstanceMethods
 
-    before_filter :track_impression
-
-    private
-
-      def track_impression
-        RestfulMetrics::Client.add_metric("foo.bar.org", "impression", 1, restful_metrics_cookie)
-      end
-
-  end
-
-  class TestController < ApplicationController
-
-    def index
-      @distinct_id = restful_metrics_cookie
-      RestfulMetrics::Client.add_metric("foo.bar.org", "custom", 1, @distinct_id)
-      render :text => "OK"
+    def cookies
+      @cookies ||= {}
     end
 
-    def rescue_action(e); raise(e); end
+    def request
+      require 'ostruct'
+
+      struct = OpenStruct.new
+      struct.host = 'foo.com'
+      struct
+    end
   end
 
-  before(:each) do
-    ActionDispatch::Request.any_instance.stubs(:cookie_jar).returns({})
-    RestfulMetrics::Connection.any_instance.stubs(:post).returns(true)
-    RestfulMetrics::Client.set_credentials('xyz123')
+  let(:test_controller) { TestController.new }
+
+  describe '#restful_metrics_cookie' do
+    let(:result) { test_controller.send(:restful_metrics_cookie) }
+
+    it 'generates a distinct id' do
+      test_controller.expects(:generate_distinct_id).once
+      result
+    end
+
+    it 'sets the :restful_metrics cookie' do
+      result
+      test_controller.cookies[:restful_metrics].should be_present
+    end
+
+    context 'when the :analytico cookie exists' do
+      let(:existing_cookie) { '1234123412341234' }
+
+      before do
+        test_controller.cookies[:analytico] = existing_cookie
+      end
+
+      it 'returns the existing cookie' do
+        result.should == existing_cookie
+      end
+    end
+
+    context 'when the :restful_metrics cookie exists' do
+      let(:existing_cookie) { '897123408971234' }
+
+      before do
+        test_controller.cookies[:restful_metrics] = existing_cookie
+      end
+
+      it 'returns the existing cookie' do
+        result.should == existing_cookie
+      end
+    end
   end
 
-  it "should track all metrics in the stack" do
-    RestfulMetrics::Client.expects(:add_metric).twice.returns(true)
+  describe '#generate_distinct_id' do
+    let(:test_controller) { TestController.new }
+    let(:result) { test_controller.send(:generate_distinct_id) }
 
-    env = Rack::MockRequest.env_for('/', :params => {'id' => '1'})
-    status, headers, body = TestController.action(:index).call(env)
-    @response = ActionDispatch::TestResponse.new(status, headers, body)
+    it 'returns a string' do
+      result.class.should == String
+    end
 
-    @response.code.should == "200"
+    it 'returns 32 characters' do
+      result.length.should == 32
+    end
+
+    it 'returns unique ids' do
+      ids = 100.times.collect { test_controller.send(:generate_distinct_id) }
+      unique_ids = ids.sort.uniq
+
+      ids.length.should == unique_ids.length
+    end
   end
-
 end
